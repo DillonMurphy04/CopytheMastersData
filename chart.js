131b03f2798ec4c42d5543e9d260dd7adfb3ef2c
Dillon Murphy <102559213+DillonMurphy04@users.noreply.github.com>
2023-05-13 17:41:30 -0700

    All ref lines added

--- chart.js
+++ chart.js
@@ -12,9 +12,9 @@ async function drawLineChart() {
       width: window.innerWidth * 0.9,
       height: 400,
       margin: {
-        top: 15,
+        top: 55,
         right: 15,
-        bottom: 40,
+        bottom: 60,
         left: 60,
       },
     }
@@ -31,86 +31,123 @@ async function drawLineChart() {
         .attr("width", dimensions.width)
         .attr("height", dimensions.height)
     
-      const bounds = wrapper.append("g")
-        .style("transform", `translate(${
-          dimensions.margin.left
-        }px, ${
-          dimensions.margin.top
+    const bounds = wrapper.append("g")
+      .style("transform", `translate(${
+        dimensions.margin.left
+      }px, ${
+        dimensions.margin.top
+      }px)`)
+
+    const yScale = d3.scaleLinear()
+      .domain(d3.extent(impeachmentData, yAccessor))
+      .range([dimensions.boundedHeight, 0])
+
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(impeachmentData, xAccessor))
+      .range([0, dimensions.boundedWidth])
+    
+    const lineGenerator = d3.line()
+      .x(d => xScale(xAccessor(d)))
+      .y(d => yScale(yAccessor(d)))
+
+    const groups = d3.group(impeachmentData, d => d.party);
+
+    for (const [party, data] of groups) {
+      bounds.append("path")
+        .datum(data)
+        .attr("d", lineGenerator)
+        .attr("fill", "none")
+        .attr("stroke", party === "dem" ? d3.rgb(56, 160, 232) : party === "rep" ? d3.rgb(232, 16, 23) : d3.rgb(0, 0, 0))
+        .attr("stroke-width", 2.75);
+    }
+
+    const yAxisGenerator = d3.axisLeft()
+      .scale(yScale)
+
+    const yAxis = bounds.append("g")
+      .call(yAxisGenerator)
+
+    const xAxisGenerator = d3.axisBottom()
+      .scale(xScale)
+    
+    const xAxis = bounds.append("g")
+      .call(xAxisGenerator)
+        .style("transform", `translateY(${
+          dimensions.boundedHeight
         }px)`)
+    
+    const colorAccessor = d => d.party
+      
+    const colorScale = d3.scaleOrdinal()
+      .domain(["dem", "rep", "ind"])
+      .range([d3.rgb(56, 160, 232, 0.4), d3.rgb(232, 16, 23, 0.4), d3.rgb(0, 0, 0, 0.4)])
+    
+    const dots = bounds.selectAll("circle").data(pollsData)
+    dots.join("circle")
+      .attr("cx", d => xScale(xAccessor2(d)))
+      .attr("cy", d => yScale(yAccessor(d)))
+      .attr("r", 3.25)
+      .attr("fill", d => colorScale(colorAccessor(d)))
 
-        const yScale = d3.scaleLinear()
-          .domain(d3.extent(impeachmentData, yAccessor))
-          .range([dimensions.boundedHeight, 0])
+    function addLineWithTest(date, text1, text2) {
+      const line = bounds.append("line")
+        .attr("x1", xScale(dateParser(date)))
+        .attr("x2", xScale(dateParser(date)))
+        .attr("y1", -5)
+        .attr("y2", dimensions.boundedHeight)
+        .attr("stroke", "gray")
+        .attr("stroke-dasharray", "2px 4px")
+        .attr("stroke-width", 2);
 
-        const xScale = d3.scaleTime()
-          .domain(d3.extent(impeachmentData, xAccessor))
-          .range([0, dimensions.boundedWidth])
-        
-        const lineGenerator = d3.line()
-          .x(d => xScale(xAccessor(d)))
-          .y(d => yScale(yAccessor(d)))
+      const label = bounds.append("text")
+        .attr("y",  -20)
+        .attr("fill", "gray")
+        .style("font-size", "12px")
+        .style("text-anchor", "middle")
+        .append("tspan")
+          .attr("dy", "0em")
+          .text(text1)
+          .attr("x", xScale(dateParser(date)))
+        .append("tspan")
+          .attr("dy", "1em")
+          .text(text2)  
+          .attr("x", xScale(dateParser(date)))
+    }
+    addLineWithTest("2019-04-18", "Mueller report", "made public")
+    addLineWithTest("2019-07-24", "Mueller testified", "before Congress")
+    addLineWithTest("2019-12-18", "House voted to", "impeach Trump")
+    addLineWithTest("2020-02-05", "Senate voted to", "acquit Trump")
 
-        const groups = d3.group(impeachmentData, d => d.party);
+    const line = bounds.append("line")
+      .attr("x1", xScale(dateParser("2019-09-24")))
+      .attr("x2", xScale(dateParser("2019-09-24")))
+      .attr("y1", 0)
+      .attr("y2", dimensions.boundedHeight)
+      .attr("stroke", "gray")
+      .attr("stroke-dasharray", "2px 4px")
+      .attr("stroke-width", 2);
 
-        for (const [party, data] of groups) {
-          bounds.append("path")
-            .datum(data)
-            .attr("d", lineGenerator)
-            .attr("fill", "none")
-            .attr("stroke", party === "dem" ? d3.rgb(56, 160, 232) : party === "rep" ? d3.rgb(232, 16, 23) : d3.rgb(0, 0, 0))
-            .attr("stroke-width", 2.75);
-        }
-
-        const yAxisGenerator = d3.axisLeft()
-          .scale(yScale)
-
-        const yAxis = bounds.append("g")
-          .call(yAxisGenerator)
-
-        const xAxisGenerator = d3.axisBottom()
-          .scale(xScale)
-        
-        const xAxis = bounds.append("g")
-          .call(xAxisGenerator)
-            .style("transform", `translateY(${
-              dimensions.boundedHeight
-            }px)`)
-        
-        const colorAccessor = d => d.party
-          
-        const colorScale = d3.scaleOrdinal()
-          .domain(["dem", "rep", "ind"])
-          .range([d3.rgb(56, 160, 232, 0.4), d3.rgb(232, 16, 23, 0.4), d3.rgb(0, 0, 0, 0.4)])
-        
-        const dots = bounds.selectAll("circle").data(pollsData)
-        dots.join("circle")
-            .attr("cx", d => xScale(xAccessor2(d)))
-            .attr("cy", d => yScale(yAccessor(d)))
-            .attr("r", 3.25)
-            .attr("fill", d => colorScale(colorAccessor(d)))
-
-        const line1 = bounds.append("line")
-            .attr("x1", xScale(dateParser("2019-04-18")))
-            .attr("x2", xScale(dateParser("2019-04-18")))
-            .attr("y1", 15)
-            .attr("y2", dimensions.boundedHeight)
-            .attr("stroke", "gray")
-            .attr("stroke-dasharray", "2px 4px")
-            .attr("stroke-width", 2);
-
-        const label1 = bounds.append("text")
-            .attr("y",  -5)
-            .attr("fill", "gray")
-            .style("font-size", "12px")
-            .style("text-anchor", "middle")
-            .append("tspan")
-              .attr("dy", "0em")
-              .text("Mueller report")
-              .attr("x", xScale(dateParser("2019-04-18")))
-            .append("tspan")
-              .attr("dy", "1em")
-              .text("made public")  
-              .attr("x", xScale(dateParser("2019-04-18")))
+  const label = bounds.append("text")
+    .attr("y",  -30)
+    .attr("fill", "gray")
+    .style("font-size", "12px")
+    .style("text-anchor", "middle")
+    .append("tspan")
+      .attr("dy", "0em")
+      .text("Pelosi")
+      .attr("x", xScale(dateParser("2019-09-24")))
+    .append("tspan")
+      .attr("dy", "1em")
+      .text("announced")  
+      .attr("x", xScale(dateParser("2019-09-24")))
+    .append("tspan")
+      .attr("dy", "1em")
+      .text("impeachement")  
+      .attr("x", xScale(dateParser("2019-09-24")))
+    .append("tspan")
+      .attr("dy", "1em")
+      .text("inquiry")  
+      .attr("x", xScale(dateParser("2019-09-24")))
 }
 
 drawLineChart();
